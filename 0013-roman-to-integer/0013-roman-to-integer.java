1a60f7a737ebfdfa608a8090fec790a3aa49ce04
Learn-pixel <niki03.nath@gmail.com>
2026-08-01 19:06:34 +0530

    Time: 3 ms (78.06%), Space: 46.2 MB (86.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,9 +1,10 @@
 class Solution {
     public int romanToInt(String s) {
+        int n = s.length();
         int total = 0;
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < n; i++){
             int curr = value(s.charAt(i));
-            if(i < s.length() - 1 && curr < value(s.charAt(i+1))){
+            if(i < n - 1 && curr < value(s.charAt(i+1))){
                 total -= curr;
             }
             else{
